eccd944a75fca64d51496fa72dc996d28b416c12
mucandev <mucandev@gmail.com>
2024-04-27 18:14:25 +0200

    initialisation

---
+++ Print-it-JS-main_/assets/script.js
@@ -0,0 +1,27 @@
+let imgArrowLeft = document.querySelector('.arrow_left');
+console.log(imgArrowLeft)
+
+let imgArrowRight = document.querySelector('.arrow_right');
+console.log(imgArrowRight)
+
+
+
+
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
