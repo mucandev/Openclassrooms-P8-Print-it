43c6f870d9b0fa6c0fd1a9e656c3816511e69e62
mucandev <mucandev@gmail.com>
2024-04-29 18:00:30 +0200

    etape 3 bullet points

--- Print-it-JS-main_/assets/script.js
+++ Print-it-JS-main_/assets/script.js
@@ -1,15 +1,4 @@
-let sliderArrowLeft = document.querySelector('.arrow_left');
-console.log(sliderArrowLeft)
-sliderArrowLeft.addEventListener("click",() => {console.log("plic")});
-
-let sliderArrowRight = document.querySelector('.arrow_right');
-console.log(sliderArrowRight)
-sliderArrowRight.addEventListener("click", () => {console.log("ploc")});
-
-
-
-
-
+// tableau des slides
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -28,3 +17,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// nombre de slides
+console.log(`il y a ${slides.length} slides`)
+console.log(slides[0].image)
+
+// ________ VARIABLES GLOBALES
+let imgSlide = document.querySelector('.banner-img src');
+let pSlide = document.querySelector('#banner p');
+let imgPath =  "./assets/images/slideshow/"
+let index = 0;
+
+
+// Eventlisteners des flèches du slider
+let arrowLeft = document.querySelector('.arrow_left');
+console.log(arrowLeft)
+arrowLeft.addEventListener("click",() => {console.log(`clic sur ${arrowLeft.alt}`)});
+
+let arrowRight = document.querySelector('.arrow_right');
+console.log(arrowRight)
+arrowRight.addEventListener("click", () => {console.log(`clic sur ${arrowRight.alt}`)});
+
+
+// ________AJOUTER DES BULLETS POINTS AU SLIDER
+let bulletsDiv = document.querySelector('.dots');
+
+function displayBulletsSlider(){
+	for (let i=0; i<slides.length; i++) {
+		const bullet = document.createElement("p");			// créer la balise pour bullet
+		bullet.classList.add("dot");									// Ajouter la classe "dot" au bullet
+		bulletsDiv.appendChild(bullet);                              // situer le parent du nouvel élément 
+		if (i==index){
+		bullet.classList.add("dot_selected");						// Ajouter la classe "dot selected" au bullet
+		}
+	}	
+}
+displayBulletsSlider()
